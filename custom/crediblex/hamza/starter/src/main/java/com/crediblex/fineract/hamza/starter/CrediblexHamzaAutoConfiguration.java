06d003969f3d3f58be905680603e2bb95cd11b31
Hamza Farooq <hamza.farooq@crediblex.io>
2025-05-23 11:06:39 +0500

    push all changes

---
+++ custom/crediblex/hamza/starter/src/main/java/com/crediblex/fineract/hamza/starter/CrediblexHamzaAutoConfiguration.java
@@ -0,0 +1,7 @@
+package com.crediblex.fineract.hamza.starter;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+
+@AutoConfiguration
+public class CrediblexHamzaAutoConfiguration {}
