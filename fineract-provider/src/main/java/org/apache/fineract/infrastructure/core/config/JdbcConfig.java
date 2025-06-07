0137cd55008fca37f0b3a5c8071dbeeaf0b79115
Ronald Kayondo <noryak1234@gmail.com>
2025-06-07 12:52:22 +0000

    feat(CRED-78): Show all over due balance when on loan repayment screen

--- fineract-provider/src/main/java/org/apache/fineract/infrastructure/core/config/JdbcConfig.java
+++ fineract-provider/src/main/java/org/apache/fineract/infrastructure/core/config/JdbcConfig.java
@@ -6,9 +6,9 @@
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing,
  * software distributed under the License is distributed on an
  * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
@@ -18,6 +18,9 @@
  */
 package org.apache.fineract.infrastructure.core.config;
 
+import io.github.kayr.ezyquery.EzySql;
+import io.github.kayr.ezyquery.sql.ConnectionProvider;
+import io.github.kayr.ezyquery.sql.Zql;
 import org.apache.fineract.infrastructure.core.service.database.RoutingDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -35,5 +38,15 @@ public class JdbcConfig {
     @Bean
     public NamedParameterJdbcTemplate namedParameterJdbcTemplate(RoutingDataSource dataSource) {
         return new NamedParameterJdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public Zql zql(RoutingDataSource dataSource) {
+        return new Zql(ConnectionProvider.of(dataSource));
+    }
+
+    @Bean
+    public EzySql ezySql(Zql zql) {
+        return EzySql.withZql(zql);
     }
 }
