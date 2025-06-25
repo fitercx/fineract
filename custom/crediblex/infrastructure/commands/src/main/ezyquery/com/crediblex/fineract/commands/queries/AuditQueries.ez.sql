02434c867ed038dd410f6c32f635fabc038539f3
Hamza Farooq <hamza.farooq@crediblex.io>
2025-06-25 15:18:46 +0500

    CRED-72: Formats code by running spotless

--- custom/crediblex/infrastructure/commands/src/main/ezyquery/com/crediblex/fineract/commands/queries/AuditQueries.ez.sql
+++ custom/crediblex/infrastructure/commands/src/main/ezyquery/com/crediblex/fineract/commands/queries/AuditQueries.ez.sql
@@ -1,12 +1,12 @@
 -- ## dynamic:LoanChargeWaiveDetails
-SELECT 
+SELECT
     c.display_name as clientName_string,
     l.id as loanId_long,
     lc.amount as waiveOffAmount_decimal,
     lc.id as loanChargeId_long
-FROM 
+FROM
     m_loan_charge lc
-JOIN 
+JOIN
     m_loan l ON lc.loan_id = l.id
-JOIN 
+JOIN
     m_client c ON l.client_id = c.id
