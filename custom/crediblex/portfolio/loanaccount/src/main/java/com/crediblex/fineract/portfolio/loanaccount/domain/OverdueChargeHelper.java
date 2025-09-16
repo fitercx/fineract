d5aee6b3f76a327ac10aec731d22cec3a935f104
Hamza Farooq <hamza.farooq@crediblex.io>
2025-09-16 15:50:15 +0500

    CRED-100: Adds overdue charge helper

---
+++ custom/crediblex/portfolio/loanaccount/src/main/java/com/crediblex/fineract/portfolio/loanaccount/domain/OverdueChargeHelper.java
@@ -0,0 +1,31 @@
+package com.crediblex.fineract.portfolio.loanaccount.domain;
+
+import org.apache.fineract.portfolio.loanaccount.loanschedule.data.OverdueLoanScheduleData;
+import org.apache.fineract.portfolio.loanaccount.service.LoanChargeWritePlatformService;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.List;
+
+@Service
+public class OverdueChargeHelper {
+    private final LoanChargeWritePlatformService loanChargeWritePlatformService;
+
+    public OverdueChargeHelper(LoanChargeWritePlatformService loanChargeWritePlatformService) {
+        this.loanChargeWritePlatformService = loanChargeWritePlatformService;
+    }
+
+    public void applyOverdueChargesForSingleLoan(Long loanId,
+                                                 Long penaltyWaitPeriodValue,
+                                                 Boolean backdatePenalties,
+                                                 Collection<OverdueLoanScheduleData> allOverdueInstallments) {
+
+        List<OverdueLoanScheduleData> installmentsForLoan = allOverdueInstallments.stream()
+                .filter(installment -> loanId.equals(installment.getLoanId()))
+                .toList();
+
+        if (!installmentsForLoan.isEmpty()) {
+            loanChargeWritePlatformService.applyOverdueChargesForLoan(loanId, installmentsForLoan);
+        }
+    }
+}
