db57df5deeb0702948ac72cb67c121440386e23d
hayederr <haider@crediblex.io>
2025-10-01 05:01:38 +0500

    fix: Reorder import statements in SavingsJournalEntryCreatedBusinessEvent

--- custom/crediblex/accounting/journalentry/src/main/java/com/crediblex/fineract/accounting/journalentry/SavingsJournalEntryCreatedBusinessEvent.java
+++ custom/crediblex/accounting/journalentry/src/main/java/com/crediblex/fineract/accounting/journalentry/SavingsJournalEntryCreatedBusinessEvent.java
@@ -1,8 +1,8 @@
 package com.crediblex.fineract.accounting.journalentry;
 
 import org.apache.fineract.accounting.journalentry.domain.JournalEntry;
-import org.apache.fineract.infrastructure.event.business.domain.journalentry.JournalEntryBusinessEvent;
 import org.apache.fineract.infrastructure.event.business.domain.NoExternalEvent;
+import org.apache.fineract.infrastructure.event.business.domain.journalentry.JournalEntryBusinessEvent;
 
 public class SavingsJournalEntryCreatedBusinessEvent extends JournalEntryBusinessEvent implements NoExternalEvent {
 
