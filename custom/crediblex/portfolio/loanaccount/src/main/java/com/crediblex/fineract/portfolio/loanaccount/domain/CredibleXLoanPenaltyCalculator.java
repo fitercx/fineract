9091b9da98840430084c4d88693931f5156c383c
Julius Oketayot <julius.poketayot@gmail.com>
2026-01-09 09:10:34 +0300

    Bug/CRED-100: Fix penalty and installment issues

--- custom/crediblex/portfolio/loanaccount/src/main/java/com/crediblex/fineract/portfolio/loanaccount/domain/CredibleXLoanPenaltyCalculator.java
+++ custom/crediblex/portfolio/loanaccount/src/main/java/com/crediblex/fineract/portfolio/loanaccount/domain/CredibleXLoanPenaltyCalculator.java
@@ -137,9 +137,9 @@ public class CredibleXLoanPenaltyCalculator {
 
         // Step 3: Collect penalties that are after the latest applicable charge due date
         return loanCharges.stream().filter(LoanChargeData::isPenalty) // only penalties
-                .filter(charge -> charge.getDueDate() != null && !charge.getDueDate().isBefore(latestApplicableDueDate)) // after
-                                                                                                                         // latest
-                                                                                                                         // applicable
+                .filter(charge -> charge.getDueDate() != null && charge.getDueDate().isAfter(latestApplicableDueDate)) // after
+                                                                                                                       // latest
+                                                                                                                       // applicable
                 .sorted(Comparator.comparing(LoanChargeData::getDueDate)) // ascending due date
                 .collect(Collectors.toList());
     }
