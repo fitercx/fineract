4a56f25880c7c63c67416a3c8fe835ccca28bc8d
hayederr <haider@crediblex.io>
2026-06-08 12:39:43 +0500

    chore: Remove unused import statement from OdooJournalEntryService

--- custom/crediblex/integration/odoo/src/main/java/com/crediblex/fineract/integration/odoo/service/OdooJournalEntryService.java
+++ custom/crediblex/integration/odoo/src/main/java/com/crediblex/fineract/integration/odoo/service/OdooJournalEntryService.java
@@ -28,7 +28,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.fineract.accounting.journalentry.domain.JournalEntry;
