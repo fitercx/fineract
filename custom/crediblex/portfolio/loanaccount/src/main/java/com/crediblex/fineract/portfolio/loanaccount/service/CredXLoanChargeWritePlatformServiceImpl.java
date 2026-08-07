53d2c2592637a8b1c096b97519d718926e7aa100
saurav-solanki-cx <saurav.solanki@crediblex.io>
2026-08-07 17:53:40 +0530

    fix(LMS-113): LPI Job Improvement

--- custom/crediblex/portfolio/loanaccount/src/main/java/com/crediblex/fineract/portfolio/loanaccount/service/CredXLoanChargeWritePlatformServiceImpl.java
+++ custom/crediblex/portfolio/loanaccount/src/main/java/com/crediblex/fineract/portfolio/loanaccount/service/CredXLoanChargeWritePlatformServiceImpl.java
@@ -21,7 +21,6 @@ import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
